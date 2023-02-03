ac9fc1d74b916092f6307c27f48b28059819f0c1
JD <jdw0069@auburn.edu>
2023-02-03 11:45:12 -0600

    add state file to remote s3 bucket

--- chapter3/main.tf
+++ chapter3/main.tf
@@ -2,6 +2,16 @@ provider "aws" {
     region = "us-east-1"
 }
 
+terraform {
+ backend "s3" {
+    bucket = "terraform-up-and-running-state-jd" 
+    key = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "terraform-up-and-running-locks-jd" 
+    encrypt = true
+ }   
+}
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-up-and-running-state-jd"
 
@@ -46,4 +56,14 @@ resource "aws_dynamodb_table" "terraform_locks" {
     name = "LockID"
     type = "S"
   }
+}
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DyamoDB table"
 }
