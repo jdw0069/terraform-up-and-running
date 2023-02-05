435ebc165ca8020322083c65b0ea14e1ad06db06
JD <jdw0069@auburn.edu>
2023-02-04 18:12:45 -0600

    fmt and add default variables

--- chapter3/data-stores/mysql/providers.tf
+++ chapter3/data-stores/mysql/providers.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 terraform {
- backend "s3" {
-    bucket = "terraform-up-and-running-state-jd" 
-    key = "stage/data-stores/mysql/terraform.tfstate"
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-jd"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
     region = "us-east-1"
 
-    dynamodb_table = "terraform-up-and-running-locks-jd" 
-    encrypt = true
- }   
+    dynamodb_table = "terraform-up-and-running-locks-jd"
+    encrypt        = true
+  }
 }
--- chapter3/stage/services/webserver-cluster/variables.tf
+++ chapter3/stage/services/webserver-cluster/variables.tf
@@ -1,12 +1,14 @@
-#variable "db_remote_state_bucket" {
-#  description = "The name of the S3 bucket used for the database's remote state storage"
-#  type        = string
-#}
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket used for the database's remote state storage"
+  type        = string
+  default     = "terraform-up-and-running-state-jd"
+}
 
-#variable "db_remote_state_key" {
-#  description = "The name of the key in the S3 bucket used for the database's remote state storage"
-#  type        = string
-#}
+variable "db_remote_state_key" {
+  description = "The name of the key in the S3 bucket used for the database's remote state storage"
+  type        = string
+  default     = "terraform-up-and-running-jd"
+}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
