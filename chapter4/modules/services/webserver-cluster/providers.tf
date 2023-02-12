8bea43c737fe1d0716812dad6c51de99c47fbf02
JD <jdw0069@auburn.edu>
2023-02-12 14:54:49 -0600

    use different module structure

---
+++ chapter4/modules/services/webserver-cluster/providers.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-jd"
+    key    = "stage/services/webserver-cluster/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "terraform-up-and-running-locks-jd"
+    encrypt        = true
+  }
+}
---
+++ chapter4/stage/data-stores/mysql/providers.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-jd"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "terraform-up-and-running-locks-jd"
+    encrypt        = true
+  }
+}
