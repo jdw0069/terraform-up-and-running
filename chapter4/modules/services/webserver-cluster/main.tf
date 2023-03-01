9e63feeae6919137bd1a83af06fd7513118cf1fe
JD <jdw0069@auburn.edu>
2023-03-01 08:31:53 -0600

    update module with locals

--- chapter4/modules/services/webserver-cluster/main.tf
+++ chapter4/modules/services/webserver-cluster/main.tf
@@ -9,6 +9,14 @@ terraform {
   }
 }
 
+locals {
+  http_port = 80
+  any_port = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips = ["0.0.0.0/0"]
+}
+
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-00874d747dde814fa"
   instance_type   = var.instance_type
@@ -117,10 +125,10 @@ resource "aws_security_group" "alb" {
 
   # Allow inbound HTTP requests
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = local.http_port
+    to_port     = local.http_port
+    protocol    = local.tcp_protocol
+    cidr_blocks = local.all_ips
   }
 
   # Allow all outbound requests
