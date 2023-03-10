f6eb6a9d0a5d1216e9316e475bbf92d7f20aba12
JD <jdw0069@auburn.edu>
2023-03-09 20:17:00 -0600

    refactor module for ingress/egress rules to add more flexibility

--- chapter4/modules/services/webserver-cluster/main.tf
+++ chapter4/modules/services/webserver-cluster/main.tf
@@ -72,7 +72,7 @@ resource "aws_lb" "example" {
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port              = 80
+  port              = local.http_port
   protocol          = "HTTP"
 
   # By default, return a simple 404 page
@@ -122,22 +122,25 @@ resource "aws_lb_listener_rule" "asg" {
 
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb-sg-name"
+}
 
-  # Allow inbound HTTP requests
-  ingress {
-    from_port   = local.http_port
-    to_port     = local.http_port
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
 
-  # Allow all outbound requests
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  from_port = local.http_port
+  to_port = local.http_port
+  protocol = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port = local.any_port
+  to_port = local.any_port
+  protocol = local.any_protocol
 }
 
 data "terraform_remote_state" "db" {
